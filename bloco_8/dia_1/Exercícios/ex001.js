76ef5203c1e8fcb9fadf58fb095c49ceb24c192a
Filipe <andradefilipe2002@gmail.com>
2021-07-29 11:50:01 -0300

    Adiciona 1 exercicio dia 1 bloco 8

---
+++ bloco_8/dia_1/Exercícios/ex001.js
@@ -0,0 +1,12 @@
+const employe = (nome) => ({nomeCompleto: nome , email: `${nome.toLowerCase().split(' ').join('_')}@trybe.com` });
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drummond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(employe))
--- bloco_8/dia_1/Parte-III/HOF_structure.js
+++ bloco_8/dia_1/Parte-III/HOF_structure.js
@@ -21,3 +21,26 @@ repeat(3, (number) => {
     console.log(number, 'is even');
   }
 });
+
+// Pense agora que gostaríamos de testar quais números são ímpares. Veja como fica fácil ajustar a implementação:
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+};
+
+const isOdd = (number) => {
+  if ((number % 2) > 0) {
+    console.log(number, 'is odd');
+  }
+};
+
+repeat(3, isEven); // Testa quais números serão pares;
+repeat(3, isOdd); // Testa quais números serão ímpares;
