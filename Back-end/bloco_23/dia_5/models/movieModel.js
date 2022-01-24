ad1966f8fe4fa3ca7eb3096edf34d2da051699e3
Filipe Andrade <andradefilipe2002@gmail.com>
2022-01-24 14:38:10 -0300

    Conteúdo dia 5 bloco 23

---
+++ Back-end/bloco_23/dia_5/models/movieModel.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+const create = async ({ title, directedBy, releaseYear}) => {
+  const [result] = await connection
+   .execute(
+     "INSER INTO model_example.movies (title, directed_by, release_year) VALUES (?, ?, ?)",
+     [title, directedBy, releaseYear]
+   );
+
+   return {
+     id: result.insertId,
+   };
+};
+
+module.exports = {
+  create,
+}
+
---
+++ Back-end/bloco_23/dia_5/services/movieService.js
@@ -0,0 +1,26 @@
+const moviesModel = require('../models/movieModel');
+
+const isValid = (title, directedBy, releaseYear) => {
+  if(!title || typeof title !== 'string') return false;
+  if(!releaseYear || typeof releaseYear !== 'number') return false;
+  if(!directedBy || typeof directedBy !== 'string') return false;
+
+  return true;
+}
+
+const create = async ({ title, directedBy, releaseYear}) => {
+  const isMovieValid = isValid(title, directedBy, releaseYear);
+
+  if(!isMovieValid) return false;
+
+  const { id } = await moviesModel
+  .create({title, directedBy, releaseYear});
+
+  return {
+    id,
+  };
+};
+
+module.exports = {
+  create,
+};
